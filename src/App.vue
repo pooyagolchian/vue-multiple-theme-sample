8a34459b9894992204e142a1007bb75ada7cfaf9
pooya golchian <pooya.golchian@gmail.com>
2021-09-16 18:31:46 +0430

    add all files

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,64 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <vue-multiple-themes
+        :defaultTheme="'light'"
+        :themeColorList="[
+        'light',
+        'dark',
+        'sepia'
+        ]"
+        :changeThemeOff="true"
+    >
+    </vue-multiple-themes>
+    <h1 class="app-title">Vue.js Multiple theme sample</h1>
+    <div class="app-subtitle">You can use css class for color mode!</div>
+    <ul>
+      <li><a class="app-title" target="_blank" href="https://github.com/pooyagolchian/vue-multiple-themes">Github</a></li>
+      <li><a class="app-title" target="_blank" href="https://www.npmjs.com/package/vue-multiple-themes">NPM</a></li>
+      <li><a class="app-title" target="_blank" href="https://github.com/pooyagolchian/vue-multiple-theme-sample">Sample source code</a></li>
+    </ul>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import VueMultipleThemes from 'vue-multiple-themes'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    VueMultipleThemes
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+html {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  overflow: hidden;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+ul {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+li {
+  padding: 0 10px;
+  margin: 0;
+  list-style: none;
+}
+
+.change-theme-box {
+  font-size:30px !important;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 </style>
+
